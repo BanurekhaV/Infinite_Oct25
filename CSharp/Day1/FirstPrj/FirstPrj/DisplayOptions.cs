5cbecfb452e15695ea83ad6ff375078020988fc5
BanurekhaV <rekhasairam@hotmail.com>
2025-10-30 18:23:30 +0530

    csharp day1

---
+++ CSharp/Day1/FirstPrj/FirstPrj/DisplayOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPrj
+{
+    internal class DisplayTypes
+    {
+        //let us check various display options 
+
+        public void Display()
+        {
+            string fname, lname;
+            Console.WriteLine("Enter your FirstName :");
+            fname = Console.ReadLine();
+            Console.WriteLine("Enter your LastName :");
+            lname = Console.ReadLine();
+
+            Console.WriteLine("Your FirstName is : " + fname + " and last name is : " + lname);  //concatenation
+
+            Console.WriteLine("Your FirstName is : {0} and last name is {1} ", fname ,lname); //placeholder
+
+            Console.WriteLine($"Your FirstName is : {fname} and last name is : {lname} "); //string interpolation
+        }
+    }
+}
---
+++ CSharp/Day1/FirstPrj/FirstPrj/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+namespace FirstPrj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and Welcome to CSharp..");
+
+            //trying to call other functions of the same class
+
+            //1. non static instance function need an object of the class
+            //2. static functions does not need any object
+
+            Program program = new Program();
+            int z = program.add(5, 6);  // calling an instance function / method
+            Console.WriteLine(z);
+
+
+            //creatimg object 2
+            Program program1 = new Program();
+            Console.WriteLine(program1.add(10, 6));
+            
+            Greet();   //calling a static function of the same class 
+
+            //calling the static function of another class
+
+            NewClass.Message("Infinite");
+
+            //calling an instance method of another class
+            NewClass nc = new NewClass();
+            Console.WriteLine(nc.Hello("Kanishka")); 
+
+            //calling instance function of another class in another file
+            
+            DisplayTypes dt = new DisplayTypes();
+            dt.Display();
+            Console.ReadLine();  //holds the screen and waits for input
+        }
+
+        int add(int x, int y)  //callee
+        {
+            Console.WriteLine("hi reached add function");
+            return x + y;
+            Console.WriteLine("Going out of add");  //unreachable code
+        }
+
+        static void Greet()
+        {
+            Console.WriteLine( "Hi");
+        }
+    }
+    class NewClass
+    {
+        public static void Message(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+        public string Hello(string name)  // instance method
+        {
+            return "Hi" + name;
+        }
+    }
+    
+}
