6bb2da3af0c3fd5e75c4ceb47d6bc93903b42aff
BanurekhaV <rekhasairam@hotmail.com>
2025-12-30 11:39:00 +0530

    day5 asp

--- ASP/Exceptions_Prj/Exceptions_Prj/Global.asax.cs
+++ ASP/Exceptions_Prj/Exceptions_Prj/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
@@ -36,6 +37,22 @@ namespace Exceptions_Prj
             //Server.ClearError();
             //// Response.Write(ex.GetType());
             //Server.Transfer("apperr.html");
+
+            //to handle other unhandled exceptions if any
+           // HttpException lstex = Server.GetLastError() as HttpException;
+          //  if(lstex.GetHttpCode()==403)
+            //{
+            //    Server.Transfer("~/err.aspx");
+            //}
+
+            //logging errors onto the file
+            Exception ex = Server.GetLastError();
+            Server.ClearError();
+            string str = "";
+            str+= str + " " + ex.Message + " " +ex.Source + " "+ ex.StackTrace;
+            string path = "C:\\Infinite_Oct25\\ASP\\Errorfile.txt";
+            File.WriteAllText(path, str);
+            Server.Transfer("~/PageNotFound.aspx");
         }
 
         protected void Session_End(object sender, EventArgs e)
