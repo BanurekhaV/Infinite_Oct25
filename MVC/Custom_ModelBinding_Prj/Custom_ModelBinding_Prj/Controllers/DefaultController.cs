1b4e23fc2bc412fb50518fc505173631e1781c62
BanurekhaV <rekhasairam@hotmail.com>
2026-01-08 10:59:43 +0530

    day5 mvc

---
+++ MVC/Custom_ModelBinding_Prj/Custom_ModelBinding_Prj/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Custom_ModelBinding_Prj.Models;
+using Custom_ModelBinding_Prj.CustomBindings;
+
+namespace Custom_ModelBinding_Prj.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index([ModelBinder(typeof(CustomBinder))]CustomModel cm)
+        {
+            ViewBag.ctitle= cm.Title;
+            ViewBag.dt=cm.Date;
+            return View(cm);
+        }
+    }
+}
---
+++ MVC/Custom_ModelBinding_Prj/Custom_ModelBinding_Prj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Custom_ModelBinding_Prj.MvcApplication" Language="C#" %>
